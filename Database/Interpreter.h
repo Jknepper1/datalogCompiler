d7cfde797405ead4c92d3cb82c480212e3728198
Jknepper1 <jsknepper10@gmail.com>
2025-03-24 20:57:31 -0600

    Successfully joining rule relations

--- Database/Interpreter.h
+++ Database/Interpreter.h
@@ -61,35 +61,134 @@ class Interpreter {
             // for (Relation& r : database.getRelations()) {
             //     cout << r.toString();
             // }
-
+            
+            // TA notes:
+            // Ensure matching lowercase letters do in fact match the uppercase N
+            // So a RENAME may need to happen to make all parts of the scheme uppercase and matchable
+            // 
 
             // RULES
             // FOR PROJECT TO WORK I NEED TO MAKE SURE AN ORDERED LIST IS PASSED INTO IT
             // I MIGHT BE ABLE TO ACCOMPLISH THIS BY USING A SET AS THE PROJETLIST DATA TYPE 
             // THEN CONVERTING THAT SET INTO A VECTOR SO IT MATCHES THE RIGHT TYPE FOR SCHEME?
+            vector<Relation> tempRelations;
             for (Rule rule : datalog.getRules()) {
-                vector<Predicate> rightHand = rule.getBody();
-                vector<Relation> relations;
-                // COPIED FROM SCHEME EVALUATION ABOVE
-                // STEP 1 of Rule Evaluation
-                for (Predicate p : rightHand) {
-                    vector<string> attributes;
-                    vector<Parameter> param = p.getParams();
+                tempRelations.clear(); // Wipes the list before new temps are appended
+
+            //     vector<Predicate> rightHand = rule.getBody();
+            //      // Set left hand relation to first relation and right hand equal to next relation
+                
+            //     // COPIED FROM SCHEME EVALUATION ABOVE
+            //     // STEP 1 of Rule Evaluation
+            //     for (Predicate p : rightHand) {
+            //         vector<string> attributes;
+            //         vector<Parameter> param = p.getParams();
                     
-                    // Extracts the values of each parameter of the scheme to be inserted into the database
-                    for (Parameter p : param) {
-                        string val = p.toString();
-                        attributes.push_back(val);
+            //         // Extracts the values of each parameter of the scheme to be inserted into the database
+            //         for (Parameter p : param) {
+            //             string val = p.toString();
+            //             attributes.push_back(val);
+            //         }
+            //         // Creates the temp relation
+            //         Relation temp(p.getName(), attributes);
+            //         // How do I get the facts into the relation?? I can use the scheme logic to build the relation scheme, 
+            //         // but what about the values?? Just use the facts logic.
+
+            //         // COPIED FACTS LOGIC AND ADJUSTED
+            //         for (Predicate fact : datalog.getFacts()) {
+            //             vector<string> values;
+            //             vector<Parameter> param = fact.getParams();
+        
+            //             // Fill values with parameters from fact
+            //             for (Parameter f : param) {
+            //                 string value = f.toString();
+            //                 values.push_back(value);
+            //             }
+        
+            //             // Create a tuple object with values from Parameters
+            //             Tuple tup(values);
+        
+            //             // Find the relation the fact belongs to and then add it as a tuple
+            //             if (fact.getName() == temp.getName()) {
+            //                 temp.addTuple(tup);
+            //             }
+            //         }
+            //         tempRelations.push_back(temp);
+            //     }
+                
+                // Step 1 of Eval
+
+                for (Predicate p : rule.getBody()) {
+                    Relation r = database.getRelation(p.getName()); // & reference?
+                    vector<Parameter> params = p.getParams();
+                    vector<int> projectList;
+                    unordered_map<string, int> seenVars;
+                    vector<string> renameList;
+
+                    for (size_t i = 0; i < params.size(); i++) {
+                        Parameter param = params[i];
+                        if (param.isStringLiteral()) {
+                            r = r.select(i, param.toString());
+                        }
+                        else {
+                            string varName = param.toString();
+                            if (seenVars.count(varName)) {
+                                r = r.select2(seenVars[varName], i);
+                            }
+                            else {
+                                seenVars[varName] = i;
+                                projectList.push_back(i);
+                                renameList.push_back(varName);
+                            }
+                        }
                     }
-                    // Creates a relation from 
-                    Relation temp(p.getName(), attributes);
-                    relations.push_back(temp);
-                    // How do I get the facts into the relation?? I can use the scheme logic to build the relation scheme, 
-                    // but what about the values?? Do I just use the facts logic?
+                    r = r.project(projectList);
+                    r = r.rename(renameList);
+                    tempRelations.push_back(r);
+
                 }
-                // Step 2 of Rule Evaluation
+
+                // Step 2 of eval
+                // Set result to first relation; iterate through rest of relations appending joins to result
+                Relation result = tempRelations[0];
+                if (tempRelations.size() > 1) {
+                    for (size_t i = 1; i < tempRelations.size(); i++) {
+                        result = result.join(tempRelations[i]);
+                    }
+                } // There might be excess values here or duplicates
+                // cout << result.toString();
+
+                // Step 3 of eval
+                vector<int> projectList;
+                vector<string> headAttributes;
+
+                // Get all attributes from head pred into comparison list
+                for (Parameter p : rule.getHead().getParams()) {
+                    headAttributes.push_back(p.toString());
+                }
+
+                // Searches for each head attribute in the relation's scheme and returns it's index to be projected with
+                for (string attr : headAttributes) {
+                    for (size_t i = 0; i < result.getScheme().size(); i++) {
+                        if (result.getScheme()[i] == attr) {
+                            projectList.push_back(i);
+                        }
+                    }
+                }
+
+                result = result.project(projectList); // SEcond rule has two Gs G and g
+
+                // Step 4 of Eval
+                database.getRelation();
+
 
             }
+            // Relation population proof
+            for (Relation r : tempRelations) {
+                cout << r.toString();
+            }
+
+            
 
             // QUERIES - DOES THIS NEED TO BE DONE ON A COPY??
             for (Predicate query : datalog.getQueries()) {
